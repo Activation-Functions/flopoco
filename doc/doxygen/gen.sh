0eb78a23aa1f51fc61f2792b7b85b589c637670a
fdedinec <Florent.de-Dinechin@insa-lyon.fr>
2022-10-10 10:08:43 +0200

    -mmore attic cleanup

--- doc/doxygen/gen.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-HEADPATH="\.\./\.\./src/"
-echo "INPUT =\\ " > activeops.txt
-../../tools/EraseComments.sh ../../src/SourceFileList.txt /dev/stdout | sed "s:.*:$HEADPATH\0.hpp \\\:"  >> activeops.txt
-doxygen	Doxyfile
-rm activeops.txt
