d5f68260f65be61caaeb61f77fbe76f397b6d163
Luc Forget <luc.forget@insa-lyon.fr>
2022-11-24 14:54:39 +0100

    Updating install script for focal

--- doc/web/install_scripts/install-full-gitmaster-on-focal64.sh
+++ doc/web/install_scripts/install-full-gitmaster-on-focal64.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # install the dependencies that are available in a Debian distribution
-yes | sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install -y git cmake ninja sollya wget g++ libsollya-dev flex bison libboost-all-dev autotools-dev autoconf automake f2c libblas-dev liblapack-dev libtool liblpsolve55-dev lp-solve
+ 
+yes | sudo apt update && \
+      sudo DEBIAN_FRONTEND=noninteractive apt install -y autoconf automake autotools-dev bison f2c flex git gpg g++ libblas-dev libboost-all-dev liblapack-dev liblpsolve55-dev libsollya-dev libtool lp-solve ninja-build pkg-config sollya wget && \
+      wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null && \
+      echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null && \
+      sudo apt update && \
+      sudo DEBIAN_FRONTEND=noninteractive apt install -y kitware-archive-keyring cmake
 
 
 git clone https://gitlab.com/flopoco/flopoco
