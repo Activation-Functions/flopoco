a76ee673dc501a697a0f36edc95c1f18fe9cb7ea
fdedinec <Florent.de-Dinechin@insa-lyon.fr>
2023-06-08 15:13:20 +0200

    peglib update

--- code/VHDLOperators/src/DAG/peglib.h
+++ code/VHDLOperators/src/DAG/peglib.h
@@ -1003,6 +1003,9 @@ public:
     size_t len = static_cast<size_t>(-1);
 
     if (!for_label_) { c.cut_stack.push_back(false); }
+    auto se = scope_exit([&]() {
+      if (!for_label_) { c.cut_stack.pop_back(); }
+    });
 
     size_t id = 0;
     for (const auto &ope : opes_) {
@@ -1029,8 +1032,6 @@ public:
 
       id++;
     }
-
-    if (!for_label_) { c.cut_stack.pop_back(); }
 
     return len;
   }
@@ -2753,7 +2754,12 @@ inline size_t Holder::parse_core(const char *s, size_t n, SemanticValues &vs,
       chvs.sv_ = std::string_view(s, len);
       chvs.name_ = outer_->name;
 
-      if (!dynamic_cast<const peg::PrioritizedChoice *>(ope_.get())) {
+      auto ope_ptr = ope_.get();
+      {
+        auto tok_ptr = dynamic_cast<const peg::TokenBoundary *>(ope_ptr);
+        if (tok_ptr) { ope_ptr = tok_ptr->ope_.get(); }
+      }
+      if (!dynamic_cast<const peg::PrioritizedChoice *>(ope_ptr)) {
         chvs.choice_count_ = 0;
         chvs.choice_ = 0;
       }
@@ -4531,8 +4537,8 @@ public:
                const char *path = nullptr) const {
     if (grammar_ != nullptr) {
       const auto &rule = (*grammar_)[start_];
-      return post_process(s, n,
-                          rule.parse_and_get_value(s, n, dt, val, path, log_));
+      auto result = rule.parse_and_get_value(s, n, dt, val, path, log_);
+      return post_process(s, n, result);
     }
     return false;
   }
@@ -4678,7 +4684,7 @@ private:
 inline void enable_tracing(parser &parser, std::ostream &os) {
   parser.enable_trace(
       [&](auto &ope, auto s, auto, auto &, auto &c, auto &, auto &trace_data) {
-	auto prev_pos = std::any_cast<size_t>(trace_data);
+        auto prev_pos = std::any_cast<size_t>(trace_data);
         auto pos = static_cast<size_t>(s - c.s);
         auto backtrack = (pos < prev_pos ? "*" : "");
         std::string indent;
@@ -4802,8 +4808,8 @@ inline void enable_profiling(parser &parser, std::ostream &os) {
                      "Total counters");
             os << buff << std::endl;
 
-            snprintf(buff, BUFSIZ, "%4s  %10s  %5s  %10.2f  %10.2f  %s", "",
-                     "", "", total_success * 100.0 / grand_total,
+            snprintf(buff, BUFSIZ, "%4s  %10s  %5s  %10.2f  %10.2f  %s", "", "",
+                     "", total_success * 100.0 / grand_total,
                      total_fail * 100.0 / grand_total, "% success/fail");
             os << buff << std::endl << std::endl;
             ;
@@ -4812,8 +4818,8 @@ inline void enable_profiling(parser &parser, std::ostream &os) {
             for (auto &[name, success, fail] : stats.items) {
               auto total = success + fail;
               auto ratio = total * 100.0 / stats.total;
-              snprintf(buff, BUFSIZ, "%4zu  %10zu  %5.2f  %10zu  %10zu  %s",
-                       id, total, ratio, success, fail, name.c_str());
+              snprintf(buff, BUFSIZ, "%4zu  %10zu  %5.2f  %10zu  %10zu  %s", id,
+                       total, ratio, success, fail, name.c_str());
               os << buff << std::endl;
               id++;
             }
