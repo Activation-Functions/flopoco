5ec04232245e32e72a131a0f63dbb139d289860a
Martin Kumm <martin.kumm@cs.hs-fulda.de>
2023-11-28 18:03:20 +0100

    rounding added, testbench tbd

--- code/VHDLOperators/include/flopoco/FixFilters/FixFIRTransposed.hpp
+++ code/VHDLOperators/include/flopoco/FixFilters/FixFIRTransposed.hpp
@@ -25,7 +25,7 @@ namespace flopoco{
 		 *						If rescale=true, all the coefficients are rescaled by 1/sum(|coeffs|).
 		 * This way the output is also in [-1,1], output size is equal to input size, and the output signal makes full use of the output range.
 		*/
-    FixFIRTransposed(OperatorPtr parentOp, Target* target, int wIn, vector<int64_t> coeff, string adder_graph="", const string& graph_truncations="", const string& sa_truncations="", const int epsilon=0);
+    FixFIRTransposed(OperatorPtr parentOp, Target* target, int wIn, int wOut, vector<int64_t> coeff, string adder_graph="", const string& graph_truncations="", const string& sa_truncations="", const int epsilon=0);
 
     void parseSAtruncations(const string& sa_truncations, vector<vector<int>>& truncations);
 
