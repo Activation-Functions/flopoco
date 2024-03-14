56dbca8260124d96476ef61d6719d4ec0e3f1df5
Tom Hubrecht <tom@hubrecht.ovh>
2024-03-14 16:21:59 +0100

    feat: Add dev tooling, with nix and pre-commit

---
+++ default.nix
@@ -0,0 +1,41 @@
+{
+  sources ? import ./npins,
+  pkgs ? import sources.nixpkgs { config.allowUnfree = true; },
+}:
+
+{
+  devShell = pkgs.mkShell {
+    name = "flopoco-dev";
+
+    packages = with pkgs; [
+      # Used to compile flopoco
+      cmake
+      pkg-config
+      bison
+      boost
+      flex
+      gmp
+      libxml2
+      blas
+      mpfi
+      mpfr
+      scalp
+      sollya
+      wcpg
+
+      # Dev utilities
+      pre-commit
+      clang-tools
+    ];
+
+    shellHook = ''
+      if [ -e .git/hooks/pre-commit ]; then
+        # Remove existing hook
+        rm .git/hooks/pre-commit
+      fi
+
+      # Install pre-commit hooks
+      pre-commit install
+    '';
+  };
+}
