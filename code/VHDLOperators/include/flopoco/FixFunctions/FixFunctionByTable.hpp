1b74ca50bde652d42a062f2d022339d25fe22fe9
Tom Hubrecht <tom@hubrecht.ovh>
2024-03-15 11:18:24 +0100

    chore(FixFunctions): Reformat header files

--- code/VHDLOperators/include/flopoco/FixFunctions/FixFunctionByTable.hpp
+++ code/VHDLOperators/include/flopoco/FixFunctions/FixFunctionByTable.hpp
@@ -1,40 +1,36 @@
 #ifndef FixFunctionByTable_HPP
 #define FixFunctionByTable_HPP
-#include <vector>
 
-#include "flopoco/InterfacedOperator.hpp"
-#include "flopoco/Tables/Table.hpp"
 #include "flopoco/FixFunctions/FixFunction.hpp"
 #include "flopoco/Tables/TableOperator.hpp"
 
-namespace flopoco{
+#include <vector>
 
-	/** The FixFunctionByTable class */
-	class FixFunctionByTable : public TableOperator
-	{
-	public:
-		/**
+namespace flopoco
+{
+
+  /** The FixFunctionByTable class */
+  class FixFunctionByTable : public TableOperator {
+    public:
+    /**
 			 The FixFunctionByTable constructor. For the meaning of the parameters, see FixFunction.hpp
 		 */
 
-		FixFunctionByTable(OperatorPtr parentOp, Target* target, string func, bool signedIn, int lsbIn, int lsbOut);
+    FixFunctionByTable(OperatorPtr parentOp, Target* target, string func, bool signedIn, int lsbIn, int lsbOut);
 
-		/**
+    /**
 		 * FixFunctionByTable destructor
 		 */
-		~FixFunctionByTable();
+    ~FixFunctionByTable();
 
-		void emulate(TestCase * tc);
+    void emulate(TestCase* tc);
 
-		/** Factory method that parses arguments and calls the constructor */
-		static OperatorPtr parseArguments(OperatorPtr parentOp, Target *target, vector<string> &args, UserInterface& ui);
-
-	protected:
-
-		FixFunction *f;
-		unsigned wR;
-	};
-
-}
+    /** Factory method that parses arguments and calls the constructor */
+    static OperatorPtr parseArguments(OperatorPtr parentOp, Target* target, vector<string>& args, UserInterface& ui);
+    protected:
+    FixFunction* f;
+    unsigned wR;
+  };
+}  // namespace flopoco
 
 #endif
