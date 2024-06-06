4dd88fb6cca56492e8de1d4f859e96d920a02780
Pierre Cochard <pierre.cochard@inria.fr>
2024-06-06 17:46:56 +0200

    [autotests] code cleanup and corrections, now using serde

--- tools/autotests-html/src/main.rs
+++ tools/autotests-html/src/main.rs
@@ -1,82 +1,114 @@
-
-// Usage: autotests-html:
-// - run flopoco autotest operator=all testlevel=0
-// - get the output summary result in /tmp
-// - parse csv, convert to html
-// - for every row, parse total/generation and total/simulation
-// - if they're the same, highlight the cell green
-// - otherwise, highlight the cell red
-
-
 use std::fs;
 use build_html::{Html, HtmlContainer, TableCell, TableCellType, TableRow};
+use serde::{Serialize, Deserialize, Deserializer};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OperatorResults {
+    #[serde(rename(deserialize = "Operator Name"))]
+    id: String,
+    // Note: the generated .csv file has ', ' as delimiter
+    // which is impossible to set in the reader's options,
+    // so it doesn't take the whitespace into account:
+    #[serde(rename(deserialize = " Total Tests"))]
+    #[serde(deserialize_with = "deserialize_trim")]
+    ntests: i32,
+    #[serde(rename(deserialize = " Generation OK"))]
+    #[serde(deserialize_with = "deserialize_trim")]
+    gen: i32,
+    // Also Note: it's OK for 'Generation OK'
+    // but 'Ok' for 'Simulation OK'... (TODO)
+    #[serde(rename(deserialize = " Simulation Ok"))]
+    #[serde(deserialize_with = "deserialize_trim")]
+    sim: i32
+}
+
+fn deserialize_trim<'de, D>(deserializer: D) -> Result<i32, D::Error>
+    where D: Deserializer<'de>
+{
+    Ok(String::deserialize(deserializer)
+        .unwrap()
+        .trim()
+        .parse::<i32>()
+        .unwrap()
+    )
+}
+
+fn create_cell(txt: impl ToString, color: &str) -> TableCell {
+    TableCell::new(TableCellType::Data)
+        .with_paragraph_attr(
+            txt.to_string().trim(),
+            [("style", format!("color:{color}").as_str())]
+        )
+}
 
 fn main() {
     let mut html_table = build_html::Table::new();
-    // Read .csv file
-    let fcsv = fs::read_to_string("summary.csv")
+    // Read .csv file as String, from current directory:
+    let csv = fs::read_to_string("summary.csv")
         .expect("Could not open/read summary.csv file")
     ;
-    let mut rdr = csv::ReaderBuilder::new()
+    // Parse .csv file:
+    let mut reader = csv::ReaderBuilder::new()
         .delimiter(b',')
-        .from_reader(fcsv.as_bytes())
+        .from_reader(csv.as_bytes())
     ;
-    match rdr.headers() {
+    match reader.headers() {
     // Parse and add headers:
         Ok(record) => {
-            let hdr: Vec<&str> = record.iter().collect();
-            html_table.add_header_row(hdr);
+            let header: Vec<&str> = record.iter().collect();
+            html_table.add_header_row(header);
         }
         Err(..) => ()
     }
-    // Collect records:
-    rdr.records().into_iter().for_each(|r| match r {
-        Ok(record) => {
-            // Get generation cell and
-            // Get 'Total'
-            // println!("record = {:?}", record);
-            let n = record[0].parse::<String>().unwrap();
-            let t = record[1].trim().parse::<i32>().unwrap();
-            let g = record[2].trim().parse::<i32>().unwrap();
-            let s = record[3].trim().parse::<i32>().unwrap();
-            let cn = TableCell::new(TableCellType::Data)
-                .with_paragraph(n)
-            ;
-            let ct = TableCell::new(TableCellType::Data)
-                .with_paragraph(t)
-            ;
-            let mut cg = TableCell::new(TableCellType::Data);
-            let mut cs = TableCell::new(TableCellType::Data);
-            if t > 0 {
-                if g == t {
-                    // get the cell, color it green
-                    cg.add_paragraph_attr(record[2].trim(), [("style", "color:green")]);
-                } else {
-                    // get the cell, color it red...
-                    cg.add_paragraph_attr(record[2].trim(), [("style", "color:red")]);
-                }
-                // Same for 'Simulation'
-                if s == t {
-                    cs.add_paragraph_attr(record[3].trim(), [("style", "color:green")]);
-                } else {
-                    cs.add_paragraph_attr(record[3].trim(), [("style", "color:red")]);
-                }
+    for record in reader.deserialize() {
+        // For each row:
+        let op: OperatorResults = record.unwrap();
+        let mut c0 = TableCell::new(TableCellType::Data)
+            .with_paragraph(&op.id);
+        let mut c1 = TableCell::new(TableCellType::Data)
+            .with_paragraph(op.ntests);
+        let mut c2 = TableCell::new(TableCellType::Data)
+            .with_paragraph(op.gen);
+        let mut c3 = TableCell::new(TableCellType::Data)
+            .with_paragraph(op.sim);
+        if op.ntests > 0 {
+            // If autotests are available
+            let mut gen_ok = false;
+            let mut sim_ok = false;
+            if op.gen == op.ntests {
+                // If all generation tests succeed:
+                c2 = create_cell(op.gen, "green");
+                gen_ok = true;
             } else {
-                cg.add_paragraph(record[2].trim());
-                cs.add_paragraph(record[3].trim());
+                c2 = create_cell(op.gen, "red");
             }
-            // Add body row
-            html_table.add_custom_body_row(
-                TableRow::new()
-                    .with_cell(cn)
-                    .with_cell(ct)
-                    .with_cell(cg)
-                    .with_cell(cs)
-            );
+            if op.sim == op.ntests {
+                // If all simulation tests succeed:
+                c3 = create_cell(op.sim, "green");
+                sim_ok = true;
+            } else {
+                c3 = create_cell(op.sim, "red");
+            }
+            if gen_ok && sim_ok {
+                // If both Generation and Simulation are OK,
+                // Highlight very cell in green
+                c0 = create_cell(op.id, "green");
+                c1 = create_cell(op.ntests, "green");
+            } else {
+                // Otherwise, set to red
+                c0 = create_cell(op.id, "red");
+                c1 = create_cell(op.ntests, "red");
+            }
         }
-        Err(..) => (),
-    });
+        html_table.add_custom_body_row(
+            TableRow::new()
+                .with_cell(c0)
+                .with_cell(c1)
+                .with_cell(c2)
+                .with_cell(c3)
+        );
+    }
     let html = html_table.to_html_string();
     fs::write("autotests.html", &html).expect("Could not write HTML output");
-    println!("{}", &html);
+    println!("'autotests.html' successfully generated!");
 }
