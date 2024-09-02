e1b5bab4542e4133ac40c3fcda39ba540a4e9f5e
Pierre Cochard <pierre.cochard@inria.fr>
2024-09-02 14:46:59 +0200

    [merge] update install/docker scripts

--- doc/web/install_scripts/install-full-gitmaster-fedora.sh
+++ doc/web/install_scripts/install-full-gitmaster-fedora.sh
@@ -20,36 +20,6 @@ cd ..
 # Build flopoco
 git clone https://gitlab.com/flopoco/flopoco
 cd flopoco
-mkdir build
-cd build
-
-cmake -GNinja .. && \
-ninja
-
-# executables are in build/code/FloPoCoBin/
-ln -s ./code/FloPoCoBin/flopoco .
-
-# build the html documentation in doc/web. 
-./flopoco BuildHTMLDoc
-
-
-# Pure luxury: bash autocompletion. 
-./flopoco BuildAutocomplete
-
-mkdir ~/.bash_completion.d/
-# Ensure that update on the file will be updated on the computer ...
-mv flopoco_autocomplete ~/.bash_completion.d/flopoco
-
-# ... But won't add another rudundant line in the .bashrc
-if [[ ! -d "~/.bash_completion.d/flopoco" ]] then	
-	# The following line should be called only once
-	echo ". ~/.bash_completion.d/flopoco" >> ~/.bashrc
-fi
-
-# Now show the list of operators
-./flopoco  
-
-echo
-echo "If you saw the command-line help of flopoco, welcome to FloPoCo !"
+make
 
 
