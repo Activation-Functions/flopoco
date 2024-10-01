d802da13749fc8338a1e79c9d69e5302d3f3e361
Orégane Desrentes <oregane.desrentes@insa-lyon.fr>
2024-10-01 13:34:45 +0200

    Refactor the code a bit so functions are defined only once

--- code/VHDLOperators/src/FixFunctions/FixFunctionHelper.cpp
+++ code/VHDLOperators/src/FixFunctions/FixFunctionHelper.cpp
@@ -40,13 +40,46 @@ TestList generateFixFunctionUnitTest(int testLevel, int methodSize) {
 	TestList testStateList;
 	vector<pair<string,string>> paramList;
 
+	// List of functions
+	vector<string> function;
+	vector<bool> signedIn;
+	vector<int> msbOut;
+	vector<bool> tableCompression;
+	vector<bool> scaleOutput; // multiply output by (1-2^lsbOut) to prevent it  reaching 2^(msbOut+1) due to faithful rounding
+
+	// List of parameters for the size of the input
+	vector<int> input_size;
+	vector<int> degree;
+
+	// Same functions for all the test levels
+	
+	function.push_back("log(x/2+1)"); // f positive, increasing, concave
+	signedIn.push_back(false);
+	msbOut.push_back(-1);
+	scaleOutput.push_back(false);
+	tableCompression.push_back(true);
+
+	function.push_back("1/(x+1)"); // f positive, decreasing, convex
+	signedIn.push_back(false); 
+	msbOut.push_back(0);
+	scaleOutput.push_back(false); // input in [0,1) output in [0.5,1] but we don't want scaleOutput
+	tableCompression.push_back(true);
+
+	function.push_back("acos(x*0.8)-pi/2"); // f negative, decreasing, concave
+	signedIn.push_back(false);
+	msbOut.push_back(-1);
+	scaleOutput.push_back(false);
+	tableCompression.push_back(true);
+
+	function.push_back("sinh(x)-1"); // f negative, increasing, convex
+	signedIn.push_back(false);
+	msbOut.push_back(-1);
+	scaleOutput.push_back(true);
+	tableCompression.push_back(true);
+
+
 	if(testLevel >= TestLevel::EXHAUSTIVE) 
-	{ // The exhaustive unit tests
-		vector<string> function;
-		vector<bool> signedIn;
-		vector<int> msbOut;
-		vector<bool> tableCompression;
-		vector<bool> scaleOutput;			// multiply output by (1-2^lsbOut) to prevent it  reaching 2^(msbOut+1) due to faithful rounding
+	{ // The exhaustive unit tests with extra functions
 
 		// This is a regression test that used to trigger a compression bug
 		function.push_back("(2/(2-x)-1)"); // f positive, increasing, convex		
@@ -62,12 +95,6 @@ TestList generateFixFunctionUnitTest(int testLevel, int methodSize) {
 		scaleOutput.push_back(true); // input in [0,1) output in [0, 1) : need scaleOutput
 		tableCompression.push_back(true);
 
-		function.push_back("1/(x+1)"); // f positive, decreasing, convex
-		signedIn.push_back(false); 
-		msbOut.push_back(0);
-		scaleOutput.push_back(false); // input in [0,1) output in [0.5,1] but we don't want scaleOutput
-		tableCompression.push_back(true);
-
 		function.push_back("sin(pi/4*x)"); // f positive, increasing, concave
 		signedIn.push_back(false);
 		msbOut.push_back(-1);
@@ -80,7 +107,6 @@ TestList generateFixFunctionUnitTest(int testLevel, int methodSize) {
 		scaleOutput.push_back(true);
 		tableCompression.push_back(true);
 	
-//#if 0
 		// It seems we don't manage yet the case when the function may get negative
 		// but the code has been elegantly fixed: it now THROWERRORs
 		function.push_back("log(0.5+x)");  // f negative then positive, increasing, concave
@@ -88,44 +114,27 @@ TestList generateFixFunctionUnitTest(int testLevel, int methodSize) {
 		msbOut.push_back(-1);
 		scaleOutput.push_back(true);
 		tableCompression.push_back(true);
-//#endif
 
-
-		// Piecewise poly functions
-		//functionList.push_back("sin(pi/2*x)");
-		//functionList.push_back("log(x+0.5)"); // a function that gets negative
-
-		//functionList.push_back("sin(x)");
 		function.push_back("sin(x)"); // f positive, increasing, concave
 		signedIn.push_back(true);
 		msbOut.push_back(0);
 		scaleOutput.push_back(false);
 		tableCompression.push_back(true);
 
-		//functionList.push_back("exp(x)");
 		function.push_back("exp(x)");
 		signedIn.push_back(true);
 		msbOut.push_back(2);
 		scaleOutput.push_back(false);
 		tableCompression.push_back(true);
 
-		//functionList.push_back("log(x+1)");
 		function.push_back("log(x+1)"); // f positive, increasing, concave
 		signedIn.push_back(false);
 		msbOut.push_back(-1);
 		scaleOutput.push_back(false);
 		tableCompression.push_back(true);
-		
-		//functionList.push_back("tanh(4*x)");
+
 		function.push_back("tanh(4*x)"); // f negative then positive, increasing, convex then concave
 		signedIn.push_back(true);
-		msbOut.push_back(-1);
-		scaleOutput.push_back(false);
-		tableCompression.push_back(true);
-
-		//functionList.push_back("log(x/2+1)");
-		function.push_back("log(x/2+1)"); // f positive, increasing, concave
-		signedIn.push_back(false);
 		msbOut.push_back(-1);
 		scaleOutput.push_back(false);
 		tableCompression.push_back(true);
@@ -135,12 +144,6 @@ TestList generateFixFunctionUnitTest(int testLevel, int methodSize) {
 		function.push_back("cosh(x-1.7)+1"); // f positive, decreasing, convex with output in (2;4)
 		signedIn.push_back(false);
 		msbOut.push_back(2);
-		scaleOutput.push_back(false);
-		tableCompression.push_back(true);
-
-		function.push_back("acos(x*0.8)-pi/2"); // f negative, decreasing, concave
-		signedIn.push_back(false);
-		msbOut.push_back(-1);
 		scaleOutput.push_back(false);
 		tableCompression.push_back(true);
 
@@ -156,12 +159,6 @@ TestList generateFixFunctionUnitTest(int testLevel, int methodSize) {
 		scaleOutput.push_back(false);
 		tableCompression.push_back(true);
 
-		function.push_back("sinh(x)-1"); // f negative, increasing, convex
-		signedIn.push_back(false);
-		msbOut.push_back(-1);
-		scaleOutput.push_back(true);
-		tableCompression.push_back(true);
-
 		function.push_back("cos(x*pi/2)"); // f positive decreasing concave
 		signedIn.push_back(false);
 		msbOut.push_back(-1);
@@ -175,126 +172,67 @@ TestList generateFixFunctionUnitTest(int testLevel, int methodSize) {
 		scaleOutput.push_back(false);
 		tableCompression.push_back(true);
 
-
 		// Figure out which size to test all these functions on
-
-		vector<int> input_size;
-		vector<int> degree;
-
-		if (methodSize == 0) {
+		if (methodSize == 0) {  // PlainTable
 			input_size.push_back(-8);
 			degree.push_back(1);
 			input_size.push_back(-10);
 			degree.push_back(1);
 		}
 		
+		// Everything
 		input_size.push_back(-12);
 		degree.push_back(1);
 
-		if (methodSize > 0) {
+		if (methodSize > 0) {  // Everything except PlainTable
 			input_size.push_back(-14);
 			degree.push_back(2);
 			input_size.push_back(-24);
 			degree.push_back(3);
 		}
 
-		if (methodSize==2) {
+		if (methodSize==2) { // Only PiecewisePoly
 			input_size.push_back(-30);
 			degree.push_back(5);
 			input_size.push_back(-32);
 			degree.push_back(5);
 		}
-
-		for (size_t j= 0; j < input_size.size(); j++) {
-			for (size_t i =0; i<function.size(); i++) {
-				paramList.clear();
-				string f = function[i];
-				int lsbOut = input_size[j] + msbOut[i]; // to have inputSize=outputSize
-				if (scaleOutput[i]) {
-					f = "(1-1b"+to_string(lsbOut) + ")*(" + f + ")";
-				}
-				paramList.push_back(make_pair("f", f));
-				paramList.push_back(make_pair("signedIn", to_string(signedIn[i]) ) );
-				paramList.push_back(make_pair("lsbIn", to_string(input_size[j])));
-				paramList.push_back(make_pair("lsbOut", to_string(lsbOut)));
-				paramList.push_back(make_pair("plainVHDL","true"));
-				paramList.push_back(make_pair("tableCompression", to_string(tableCompression[i])));
-				paramList.push_back(make_pair("d", to_string(degree[j])));
-				// Tesbench already does exaustive testing when wIn<16
-				/*if(input_size[j]>0)
-					paramList.push_back(make_pair("TestBench n=","-2"));*/
-				testStateList.push_back(paramList);
-				}
-			}
+	}
+	else
+	{
+		// Parameter size for Quick and Substantial tests
+		if (methodSize == 0) { // PlainTable
+			input_size.push_back(-8);
+			degree.push_back(1);
+		} else if (methodSize == 1) { // MultipartiteTable and SimplePoly
+			input_size.push_back(-10);
+			degree.push_back(1);
+		} else if (methodSize == 2) { // PiecewisePoly
+			input_size.push_back(-14);
+			degree.push_back(2);
 		}
-		else // quick or substantial test
-		{
-			// The whole testsuite is in Exhaustive so I give myself a bit more to test with Quick and Substantial
-			vector<string> function;
-			vector<bool> signedIn;
-			vector<int> msbOut;
-			vector<bool> tableCompression;
-			vector<bool> scaleOutput;
+	}
 
-			function.push_back("log(x/2+1)"); // f positive, increasing, concave
-			signedIn.push_back(false);
-			msbOut.push_back(-1);
-			scaleOutput.push_back(false);
-			tableCompression.push_back(true);
-
-			function.push_back("1/(x+1)"); // f positive, decreasing, convex
-			signedIn.push_back(false); 
-			msbOut.push_back(0);
-			scaleOutput.push_back(false); // input in [0,1) output in [0.5,1] but we don't want scaleOutput
-			tableCompression.push_back(true);
-
-			function.push_back("acos(x*0.8)-pi/2"); // f negative, decreasing, concave
-			signedIn.push_back(false);
-			msbOut.push_back(-1);
-			scaleOutput.push_back(false);
-			tableCompression.push_back(true);
-
-			function.push_back("sinh(x)-1"); // f negative, increasing, convex
-			signedIn.push_back(false);
-			msbOut.push_back(-1);
-			scaleOutput.push_back(true);
-			tableCompression.push_back(true);
-
-			/*function.push_back("tanh(4*x)"); // f negative then positive, increasing, convex then concave 
-			signedIn.push_back(true);
-			msbOut.push_back(-1);
-			scaleOutput.push_back(false);
-			tableCompression.push_back(true);*/
-
-		int lsbIn = -8;
-		int degree = 1;
-
-		if (methodSize == 1) { // Simple Poly and Multipartite
-			lsbIn = -10;
-		}
-
-		if (methodSize == 2) { // Piecewise Poly
-			lsbIn = -14;
-			degree = 2;
-		}
-
+	for (size_t j= 0; j < input_size.size(); j++) {
 		for (size_t i =0; i<function.size(); i++) {
 			paramList.clear();
 			string f = function[i];
-			int lsbOut = lsbIn + msbOut[i]; // to have inputSize=outputSize
+			int lsbOut = input_size[j] + msbOut[i]; // to have inputSize=outputSize
 			if (scaleOutput[i]) {
 				f = "(1-1b"+to_string(lsbOut) + ")*(" + f + ")";
 			}
 			paramList.push_back(make_pair("f", f));
 			paramList.push_back(make_pair("signedIn", to_string(signedIn[i]) ) );
-			paramList.push_back(make_pair("lsbIn", to_string(lsbIn)));
+			paramList.push_back(make_pair("lsbIn", to_string(input_size[j])));
 			paramList.push_back(make_pair("lsbOut", to_string(lsbOut)));
 			paramList.push_back(make_pair("plainVHDL","true"));
 			paramList.push_back(make_pair("tableCompression", to_string(tableCompression[i])));
-			paramList.push_back(make_pair("d", to_string(degree)));
+			paramList.push_back(make_pair("d", to_string(degree[j])));
 			testStateList.push_back(paramList);
 			}
 		}
+
+
 		REPORT(DETAIL, "Generated " << testStateList.size() << " test(s).");
 		return testStateList;
 }
